7273eb9532a810287933930aa789e0bab3a4102a
dariosanacuore <dariosanacuore2@gmail.com>
2025-11-10 15:33:40 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/**
+ * Snack 1
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo oppure in console la bici con peso minore.
+ */
+
+
+const biciclette = [
+    { nome: "Bianchi Oltre XR4", peso: 7.2 },
+    { nome: "Specialized Tarmac SL7", peso: 6.8 },
+    { nome: "Trek Émonda SLR", peso: 6.7 },
+    { nome: "Cannondale SuperSix EVO", peso: 7.0 },
+    { nome: "Pinarello Dogma F", peso: 6.9 }
+];
+
+
